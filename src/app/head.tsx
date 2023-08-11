3352303162d19979e14eec519d1e7a9a9e3f3bfb
charles-mrt <charlesmartins.ads@gmail.com>
2023-08-11 16:41:21 -0300

    refactor: remove unnecessary comment

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,8 +1,7 @@
 export const Head = () => {
   return (
     <>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      {/* <link rel="shortcut icon" href="/favicon.png" type="image/png" /> */}
+      <meta content="width=device-width, initial-scale=1" name="viewport" />     
     </>
   )
 }
