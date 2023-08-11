afe2766a94f9499a5eb79086433f54214688ce9f
charles-mrt <charlesmartins.ads@gmail.com>
2023-08-11 19:18:04 -0300

    fix: correct 'display flex' to 'flex' in Tailwind CSS style

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,7 +6,7 @@ interface HeaderPros {
 
 export const Header = ({ userName, gameName }: HeaderPros) => {
   return (
-    <header className="w-full bg-white p-2 display flex items-start justify-between">
+    <header className="w-full bg-white p-2 flex items-start justify-between">
       <div>
         <div>logo aqui</div>
         <div>{gameName && `Nome do jogo: ${gameName}`}</div>
