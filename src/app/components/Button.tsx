7cd53a470ce2720f608d82857c71a79b33216db2
charles-mrt <charlesmartins.ads@gmail.com>
2023-08-16 14:51:12 -0300

    feat: implemented button component

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,16 @@
+interface ButtonProps {
+  name: string;
+  onClickHandler?: () => void;
+}
+
+export const Button = ({ name, onClickHandler }: ButtonProps) => {
+  return (
+    <button
+      onClick={onClickHandler}
+      type="submit"
+      className="w-96 bg-violet-700 rounded-md border-violet-700 py-4 px-6 text-white font-bold text-base text-center hover:bg-violet-800 transition-colors"
+    >
+      {name}
+    </button>
+  )
+}
