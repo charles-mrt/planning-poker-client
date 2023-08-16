f6f2dcd0dba6ec1f834011d88645b64e2216a7e2
charles-mrt <charlesmartins.ads@gmail.com>
2023-08-16 14:36:32 -0300

    added userName and gameName props as optional and added logo as  image

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,17 +1,21 @@
 
-interface HeaderPros {
-  userName: string;
-  gameName: string;
+import Image from 'next/image'
+import logo from '/public/assets/logo.svg'
+
+interface HeaderProps {
+  userName?: string;
+  gameName?: string;
 }
 
-export const Header = ({ userName, gameName }: HeaderPros) => {
+export const Header = ({ userName, gameName }: HeaderProps) => {
+
   return (
-    <header className="w-full bg-white p-2 flex items-start justify-between">
+    <header className="w-full max-h-16 bg-white p-2 flex items-start justify-between absolute left-0 top-0">
       <div>
-        <div>logo aqui</div>
-        <div>{gameName && `Nome do jogo: ${gameName}`}</div>
+        <Image src={logo} alt="logo planning poker"/>
+        {gameName && <div>{`Nome do jogo: ${gameName}`}</div>}
       </div>
       <div>{userName}</div>
     </header>
-  )
-}
+  );
+};
