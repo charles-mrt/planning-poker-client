9be003e2fa64ee0a7296bbbf6df277d46dbb9c8d
charles-mrt <charlesmartins.ads@gmail.com>
2023-08-17 14:55:34 -0300

    feat: Added icons for user and game

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,9 @@
 
 import Image from 'next/image'
 import logo from '/public/assets/logo.svg'
-
+import {Dices, User} from 'lucide-react'
 interface HeaderProps {
-  userName?: string;
+  userName?: string | null;
   gameName?: string;
 }
 
@@ -11,11 +11,22 @@ export const Header = ({ userName, gameName }: HeaderProps) => {
 
   return (
     <header className="w-full max-h-16 bg-white p-2 flex items-start justify-between absolute left-0 top-0">
+      
       <div>
         <Image src={logo} alt="logo planning poker"/>
-        {gameName && <div>{`Nome do jogo: ${gameName}`}</div>}
+        
+        <div className="flex items-center gap-1">
+          <Dices size={16} strokeWidth={1} />
+        {gameName && <span>{`Nome do jogo: ${gameName}`}</span>}
+        </div>
+        
       </div>
-      <div>{userName}</div>
+
+      <div className="flex items-center gap-1">        
+        {userName}
+        <User size={16} strokeWidth={1} />
+      </div>
+
     </header>
   )
 }
