f3b2dbf74f18ba02fc4fceb8215ad5e8a7370933
charles-mrt <charlesmartins.ads@gmail.com>
2023-08-18 14:53:32 -0300

    refactor: Clean up code

--- src/app/components/Card/CardNames.tsx
+++ src/app/components/Card/CardNames.tsx
@@ -5,27 +5,21 @@ import { Card } from './Card'
 export const CardNames = () => {
   const cardNames = ['xxs', 'x', 's', 'm', 'l', 'xl', 'xxl', '?']
   const [selectedCard, setSelectedCard] = useState<string | null>(null)
- 
 
   const handleCardClick = (cardName: string) => {
-    if (selectedCard !== "") {
-      setSelectedCard(cardName)
-      console.log("if " + cardName)
-    }
+    if (selectedCard !== "") setSelectedCard(cardName)
   }
 
   return (
-    <>
-         <div className="grid grid-cols-4 grid-rows-2 gap-5  cursor-pointer">
-        {cardNames.map((cardName, index) => (
-          <Card
-            key={index}
-            name={cardName}
-            isSelected={selectedCard === cardName}
-            onCardClick={() => handleCardClick(cardName)}
-          />
-        ))}
-      </div>
-    </>
+    <div className="grid grid-cols-4 grid-rows-2 gap-5  cursor-pointer">
+      {cardNames.map((cardName, index) => (
+        <Card
+          key={index}
+          name={cardName}
+          isSelected={selectedCard === cardName}
+          onCardClick={() => handleCardClick(cardName)}
+        />
+      ))}
+    </div>
   )
 }
