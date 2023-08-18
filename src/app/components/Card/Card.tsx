c7e7ad9cd0df9b619fe58cf96de0ae5b0c9701e2
charles-mrt <charlesmartins.ads@gmail.com>
2023-08-18 14:50:29 -0300

    style: implemented vertical positioning for highlighted card

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -10,8 +10,8 @@ export const Card = ({ name, isSelected, onCardClick }: CardProps) => {
   return (
     <div
       className={`${
-        isSelected ? "bg-indigo-200" : "bg-transparent"
-      } w-11 h-20 rounded-md border-2 border-indigo-600 text-indigo-600 flex items-center justify-center text-xl uppercase`}
+        isSelected ? "bg-indigo-200 -translate-y-2 " : "bg-transparent"
+      } w-11 h-20 rounded-md border-2 border-indigo-600 text-indigo-600 ease-in duration-150 flex items-center justify-center text-xl uppercase`}
       onClick={onCardClick}
     >
       {name}
