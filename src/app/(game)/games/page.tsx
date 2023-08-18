3a69f2240b799e0eca9f32413c556b1e88a1e059
charles-mrt <charlesmartins.ads@gmail.com>
2023-08-18 17:08:01 -0300

    refactor: useEffect to conditionally navigate and fetch last game name

--- src/app/(game)/games/page.tsx
+++ src/app/(game)/games/page.tsx
@@ -1,44 +1,37 @@
 'use client'
-import React, { useEffect, useState } from 'react'
 
+import React, { useEffect, useState } from 'react'
 import { CardNames } from '@/app/components/Card/CardNames'
 import { Header } from '@/app/components/Header'
-
 import api from '@/app/api/api'
 import { useRouter } from 'next/navigation'
 
 export default function Game() {
-
   const router = useRouter()
-
-
-  //TESTES DE ROTAS
+  const [lastGameName, setLastGameName] = useState<string>("")
   const userName = localStorage.getItem("user-name")
-  if (userName === null) {
-  router.push('/')
-  return 
-  }
-  
 
-  const [lastGameName, setLastGameName] = useState<string>("");
-  
-  useEffect(() => {
-    const fetchLastGameName = async () => {
-      try {
-        const response = await api.get(`/games`)
-        const gamesArray = response.data;
+  useEffect(() => {   
+    if (userName === null) {
+      router.push('/')
+    } else {
+      const fetchLastGameName = async () => {
+        try {
+          const response = await api.get(`/games`)
+          const gamesArray = response.data;
 
-        if (gamesArray.length > 0) {
-          const lastGame = gamesArray[gamesArray.length - 1]
-          setLastGameName(lastGame.name)                    
+          if (gamesArray.length > 0) {
+            const lastGame = gamesArray[gamesArray.length - 1]
+            setLastGameName(lastGame.name)                    
+          }
+        } catch (error) {
+          console.error("Erro ao obter o nome do último jogo:", error)
         }
-      } catch (error) {
-        console.error("Erro ao obter o nome do último jogo:", error)
       }
+      fetchLastGameName();
     }
-    fetchLastGameName();
-  }, []);
-  
+  }, [router]);
+
   return (
     <div className="w-full h-full">
       <Header userName={userName} gameName={lastGameName} />
@@ -47,7 +40,6 @@ export default function Game() {
         <h1 className="text-4xl text-gray-800">Selecione suas cartas!</h1>
         <CardNames />
       </main>  
-
     </div>
   );
 }
