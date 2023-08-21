6043e935e2a273996cfad469d294c5991d800b05
charles-mrt <charlesmartins.ads@gmail.com>
2023-08-21 18:08:38 -0300

    fix: Fixed error in data validation

--- src/app/(game)/create-game/page.tsx
+++ src/app/(game)/create-game/page.tsx
@@ -1,6 +1,17 @@
+import { CreateGameForm } from '@/app/components/Game/CreateGameForm'
+import { Header } from '@/app/components/Header'
 
-export default function Game() {
+export const metadata = { title: 'Criar jogo' }
+
+export default function createGamePage() {
+
   return (
-    <div></div>
+    <div className="w-full h-full">
+      <Header />
+      <main className="h-full w-full flex flex-col items-center justify-center gap-11">
+        <h1 className="text-4xl text-gray-800">Criar Jogo</h1>
+        <CreateGameForm />
+      </main >
+    </div>
   )
 }
