014a4d60d2566a6e5b34916c626bf4e204b00793
charles-mrt <charlesmartins.ads@gmail.com>
2023-08-21 10:19:51 -0300

    feat: component for displaying errors

---
+++ src/app/components/ErrorMessage.tsx
@@ -0,0 +1,13 @@
+import { AlertTriangle } from "lucide-react"
+
+interface ErrorMessageProps {
+  message: string;
+}
+export const ErrorMessage = ({ message }: ErrorMessageProps) => {
+  return (
+    <span className="bg-red-400 p-6 text-white rounded-md flex gap-1">
+      <AlertTriangle />
+      {message}
+    </span>
+  )
+}
