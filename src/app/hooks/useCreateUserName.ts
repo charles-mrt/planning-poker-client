e391b348ae600a079eda2667a366ac42237c2cb8
charles-mrt <charlesmartins.ads@gmail.com>
2023-08-21 10:17:06 -0300

    feat: Add user creation functionality

--- src/app/hooks/useCreateUserName.ts
+++ src/app/hooks/useCreateUserName.ts
@@ -5,6 +5,8 @@ import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 
+import api from '@/app/api/api'
+import { useState } from 'react'
 
 const createUserNameFormSchema = z.object({
   'user_name': z.string().min(2, "Mínimo 2 caracteres").max(10, "Máximo 10 caracteres")
@@ -22,22 +24,34 @@ export const useCreateUserName = () => {
   } = useForm<CreateUserNameFormData>({
     resolver: zodResolver(createUserNameFormSchema)
   })
-
+  
   const router = useRouter()
 
+  const [errorMessage, setErrorMessage] = useState('')
+  
+
   const handleCreateUserName = async (formData: CreateUserNameFormData) => {
+    
     try {
-      router.push('/games')
-      localStorage.setItem("user-name", formData.user_name)
+      const response = await api.post('/players', {
+        name: formData.user_name
+      })
+      
+      if (response.status === 201) {
+        router.push('/games')
+        localStorage.setItem("user-name", formData.user_name)
+      }
 
     } catch (error) {
       console.error("error sending data to backend", error)
+      setErrorMessage("Erro interno, tente novamente")
     }
   }
   return {
     register,
     handleSubmit,
     errors,
-    handleCreateUserName
+    handleCreateUserName,
+    errorMessage
   }
 }
