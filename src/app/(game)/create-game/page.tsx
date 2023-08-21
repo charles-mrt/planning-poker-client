6ff71d0807186a547ac35d70bd28628f2067c6ed
charles-mrt <charlesmartins.ads@gmail.com>
2023-08-21 17:56:29 -0300

    feat: create game page

--- src/app/(game)/create-game/page.tsx
+++ src/app/(game)/create-game/page.tsx
@@ -1,17 +1,6 @@
-import { CreateGameForm } from '@/app/components/Game/CreateGameForm'
-import { Header } from '@/app/components/Header'
 
-export const metadata = { title: 'Criar jogo' }
-
-export default function createGamePage() {
-
+export default function Game() {
   return (
-    <div className="w-full h-full">
-      <Header />
-      <main className="h-full w-full flex flex-col items-center justify-center gap-11">
-        <h1 className="text-4xl text-gray-800">Criar Jogo</h1>
-        <CreateGameForm />
-      </main >
-    </div>
+    <div></div>
   )
 }
