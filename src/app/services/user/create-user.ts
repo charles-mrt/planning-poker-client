fa705ac9f26ba83f1b6fd7847526277c0fb97c4f
charles-mrt <charlesmartins.ads@gmail.com>
2023-08-29 17:44:26 -0300

    feat: add new props

--- src/app/services/user/create-user.ts
+++ src/app/services/user/create-user.ts
@@ -2,18 +2,18 @@ import api from '@/app/api/api'
 
 interface UserProps {
   user_name: string
+  gameId:string
 }
 
 export const createUser = async (formData: UserProps) => {
   try {
-    const response = await api.post('/players', {
+    const response = await api.post(`games/${formData.gameId}/players`, {
       name: formData.user_name
     })
-
     if (response.status === 201) return true
   
   } catch (error) {
     console.error("error sending data to backend", error)
-    throw new Error("Erro interno, tente novamente")
+    throw new Error("Erro interno, tente novamentes")
   }
 }
