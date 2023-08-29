a0d8d01cbecb775290d035eb3db696337a3415e3
charles-mrt <charlesmartins.ads@gmail.com>
2023-08-29 17:40:27 -0300

    feat: implemented hook for game search by id

---
+++ src/app/hooks/game/use-get-game-by-id.ts
@@ -0,0 +1,31 @@
+import { getGameById } from "@/app/services/game/get-game-by-id"
+import useSWRConfig from "swr"
+
+interface useGetGameByIdProps {
+  gameId: string
+  gameName?: string
+  gameUrl?: string
+  gamePlayers?: []
+  isLoading?: boolean
+}
+
+export const useGetGameById = ({ gameId, gameName, gameUrl, gamePlayers }: useGetGameByIdProps) => {
+
+  const { data: game, error, isValidating, } = useSWRConfig(`${gameId}`, getGameById)
+
+  gameId = game?.id
+  gameName = game?.name
+  gameUrl = game?.url
+  gamePlayers = game?.players
+
+  return {
+    gameId: gameId,
+    gameName: gameName,
+    gameUrl: gameUrl,
+    gamePlayers: gamePlayers,
+    isLoading: !error && !game,
+    isError: error,
+    isValidating
+  }
+
+}
