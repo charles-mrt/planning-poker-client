741fe33143bc0db6275a83e95059179da378bd9c
charles-mrt <charlesmartins.ads@gmail.com>
2023-08-29 17:41:20 -0300

    feat: implemented the use of context

--- src/app/hooks/user/use-create-user.ts
+++ src/app/hooks/user/use-create-user.ts
@@ -2,12 +2,13 @@ import { useRouter } from 'next/navigation'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+
+import { useGameContext } from '@/app/context/Game-context'
 import { useState } from 'react'
 import { createUser } from '@/app/services/user/create-user'
-import { getLastGame } from '@/app/services/game/get-last-game'
 
 const createUserNameFormSchema = z.object({
-  'user_name': z.string().min(2, "Mínimo 2 caracteres").max(10, "Máximo 10 caracteres")
+  'user_name': z.string().min(2, "Mínimo 2 caracteres").max(15, "Máximo 15 caracteres")
 })
 
 type CreateUserNameFormData = z.infer<typeof createUserNameFormSchema>
@@ -15,6 +16,7 @@ type CreateUserNameFormData = z.infer<typeof createUserNameFormSchema>
 export const useCreateUser = () => {
   const router = useRouter()
   const [errorMessage, setErrorMessage] = useState('')
+  const { gameId } = useGameContext()
 
   const {
     register,
@@ -24,19 +26,23 @@ export const useCreateUser = () => {
     resolver: zodResolver(createUserNameFormSchema)
   })
 
-  const handleCreateUserName = async (formData: CreateUserNameFormData) => {
-    const success = await createUser(formData)
+  const handleCreateUserName = async (formData: CreateUserNameFormData, urlGameId: string) => {
+
+    const success = await createUser({
+      user_name: formData.user_name,
+      gameId: gameId ?? urlGameId
+    })
 
     if (!success) return setErrorMessage("Erro interno, tente novamente")
 
-    await redirectToGame(formData.user_name)
+    localStorage.setItem('user-name', formData.user_name)
+    await redirectToGame()
   }
 
-  const redirectToGame = async (userName: string) => {
-    const game = await getLastGame()
-      router.push(`/games/${game?.id}`)
-      localStorage.setItem("user-name", userName)
+  const redirectToGame = async () => {
+    if (gameId !== null) router.push(`/games/${gameId}`)
   }
+
 
   return {
     register,
