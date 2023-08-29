7e3550f77162f668249cd1f9be6051adeff1fbd6
charles-mrt <charlesmartins.ads@gmail.com>
2023-08-29 17:32:18 -0300

    feat: new button link component

---
+++ src/app/components/ButtonLink.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+
+
+interface ButtonLinkProps {
+  url: string
+  text: string
+}
+
+export const Buttonlink = ({ url, text }: ButtonLinkProps) => {
+  return (
+    <Link href={url} className="w-96 bg-violet-700 rounded-md border-violet-700 py-4 px-6 text-white font-bold text-base text-center hover:bg-violet-800 transition-colors">
+      {text}
+    </Link>
+  )
+}
