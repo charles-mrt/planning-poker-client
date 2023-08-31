26df55b954a5de59a6adffcad08c348c6ae47583
charles-mrt <charlesmartins.ads@gmail.com>
2023-08-31 16:14:09 -0300

    feat: Add error handling and custom message to user creation

--- src/app/services/user/create-user.ts
+++ src/app/services/user/create-user.ts
@@ -7,13 +7,13 @@ interface UserProps {
 
 export const createUser = async (formData: UserProps) => {
   try {
-    const response = await api.post(`games/${formData.gameId}/players`, {
+    const response = await api.post(`games/${formData.gameId}/players`, {      
       name: formData.user_name
     })
     if (response.status === 201) return true
   
   } catch (error) {
     console.error("error sending data to backend", error)
-    throw new Error("Erro interno, tente novamentes")
+    throw new Error("(create user) Erro interno, tente novamente")
   }
 }
