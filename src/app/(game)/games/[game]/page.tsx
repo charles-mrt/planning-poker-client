a9b764cf55a78639d5d6ab28818cfa2e81525a88
charles-mrt <charlesmartins.ads@gmail.com>
2023-09-04 14:22:38 -0300

    feat: Add new feature to reveal vote in Game component

--- src/app/(game)/games/[game]/page.tsx
+++ src/app/(game)/games/[game]/page.tsx
@@ -2,29 +2,33 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 
-import Image from 'next/image'
-import card from '/public/assets/card-cover.png'
-
-import { CardNames } from '@/app/components/Card/Card-names'
 import { Header } from '@/app/components/Header'
-import { Buttonlink } from '@/app/components/ButtonLink'
 import { ErrorMessage } from '@/app/components/Error-message'
 import { UserCreationModal } from '@/app/components/Users/User-creation-modal'
-import { Button } from '@/app/components/Button'
+import { GameNotFound } from '@/app/components/Game/Game-not-found'
+import { ConditionalButton } from '@/app/components/Game/Conditional-button'
+import { PlayersVotedCard } from '@/app/components/Card/Players-voted-card'
+import { CardsVoted } from '@/app/components/Card/Cards-voted'
 
 import { useGameContext } from '@/app/context/Game-context'
-import { useGetGameById } from '@/app/hooks/game/use-get-game-by-id'
 
+import { useGetGameById } from '@/app/hooks/game/use-get-game-by-id'
+import { handleUserVote } from '@/app/hooks/user/use-create-user-vote'
 
 interface Player {
   name: string
+  vote: string
 }
 
 export default function Game() {
 
+  const [voteCounts, setVoteCounts] = useState<{ [vote: string]: number }>({})
   const [loginFormModal, setLoginFormModal] = useState(false)
-  const { gameId: gameIdFromContext } = useGameContext()
+  const [isCardSelected, setIsCardSelected] = useState(false)
+  const [isCardRevealed, setIsCardRevealed] = useState(false)
+  const [cardValue, setIscardValue] = useState("")
 
+  const { gameId: gameIdFromContext } = useGameContext()
   const userName = localStorage.getItem('user-name')
 
   const getGameIdFromUrl = () => {
@@ -43,80 +47,121 @@ export default function Game() {
     isError,
   } = useGetGameById({ gameId: gameIdFromContext ?? gameIdFromUrl })
 
-  const showModal = () => { if (userName === null) setLoginFormModal(true) }
+  const showModal = () => {
+    if (userName === null) setLoginFormModal(true)
+  }
 
+  const handleCardRevealed = () => {
+    setIsCardRevealed(true)
+    handleUserVote()
+    cardsVoted()
+  }
 
-  const [isCardSelected, setIsCardSelected] = useState(false)
-
-  const handleCardClick = () => {
+  const getCardValue = (value: string) => {
+    setIscardValue(value)
     setIsCardSelected(true)
   }
 
+  const updateUserVote = () => {
+    if (cardValue !== "") localStorage.setItem('user-vote', cardValue)
+  }
+
+  const eraseVote = () => {
+    setIsCardRevealed(false)
+    setIsCardSelected(false)
+    setIscardValue("")
+    localStorage.setItem('user-vote', "")
+  }
+
+  const cardsVoted = () => {
+    const newVoteCounts: { [vote: string]: number } = {}
+    gamePlayers?.forEach((player: Player) => {
+      if (newVoteCounts[player.vote]) {
+        newVoteCounts[player.vote]++
+      } else {
+        newVoteCounts[player.vote] = 1
+      }
+    })
+    setVoteCounts(newVoteCounts)
+  }
 
   useEffect(() => {
     showModal()
-  }, [isCardSelected])
+    updateUserVote()
+
+  }, [isCardSelected, cardValue])
 
 
-  if (isError) return (<><ErrorMessage message='Ocorreu um erro ao carregar os dados' /></>)
+  if (isError)
+    return (
+      <div className="h-full flex items-center">
+        <ErrorMessage message='Ocorreu um erro ao carregar os dados. Tente novamente' />
+      </div>
+    )
+
+  if (!gameId) return <GameNotFound />
 
   return (
     <div className="w-full h-full">
       <Header userName={userName ?? ""} gameName={gameName} />
 
-      <main className="h-full w-full flex flex-col items-center justify-center gap-11">
+      <main className="h-full w-full flex flex-col items-center justify-center">
 
         {isLoading ? (
-          <>
-            <ErrorMessage message='Erro. Jogo não existe!' />
-            <Buttonlink url='/create-game' text='Iniciar um jogo?' />
-          </>
+
+          <p>Carregando o jogo...</p>
 
         ) : (
 
-          <>{loginFormModal && (
-            <UserCreationModal
-              urlGameId={getGameIdFromUrl()}
-              onUserCreated={() => setLoginFormModal(false)}
-            />
-          )}
+          <>
 
-            {gameId ? (
-              <div className="flex flex-col items-center">
-                <h1 className="text-4xl text-gray-800 mb-10">Selecione suas cartas!</h1>
+            {loginFormModal && (
+              <UserCreationModal
+                urlGameId={getGameIdFromUrl()}
+                onUserCreated={() => setLoginFormModal(false)}
+              />
+            )}
+
+            <div className="flex flex-col items-center">
+              <div className="h-32 flex items-center">
+                <h1 className="text-4xl text-gray-800">
+                  {!isCardSelected ? "Selecione suas cartas!" : ""}
+                </h1>
 
                 {isCardSelected && (
-                  <div>
-                    <Button name='Revelar Carta' />
-
-
-                    <ul className="m-5 flex items-start justify-around">
-
-                      {gamePlayers?.map((player: Player, index: number) => {
-                        return (
-                          <li key={index} className='flex flex-col items-center mx-2'>
-
-                            <div className=" w-10 h-16 rounded-md border-2 bg-gray-100 border-violet-700">
-                              <Image src={card} alt="card" className="object-scale-down w-full h-full" />
-                            </div>
-
-                            <span className="whitespace-pre-line w-10 text-xs text-center capitalize">
-                              {player.name}
-                            </span>
-                          </li>
-                        )
-                      })}
-                    </ul>
-                  </div>
+                  <ConditionalButton
+                    cardRevealed={isCardRevealed}
+                    handleCardRevealed={handleCardRevealed}
+                    handleEraseVote={eraseVote}
+                  />
                 )}
-                <CardNames cardSelected={handleCardClick} />
-
               </div>
-            ) : (
-              <p>Carregando o jogo...</p>
-            )}
-          </>
 
+              <ul className="my-4 flex items-start justify-around">
+                {
+                  gamePlayers?.map((gamePlayer: Player, index: number) => {
+                    return (
+                      <li key={index} className='flex flex-col items-center mx-2'>
+                        <PlayersVotedCard
+                          cardRevealed={isCardRevealed}
+                          playerVote={gamePlayer.vote}
+                          playerName={gamePlayer.name}
+                        />
+                      </li>
+                    )
+                  })
+                }
+              </ul>
+
+              <CardsVoted
+                cardRevealed={isCardRevealed}
+                voteCounts={voteCounts}
+                cardValue={getCardValue}
+              />
+
+            </div>
+
+          </>
         )}
 
       </main>
