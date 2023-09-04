32539a3d6ea995946d81347e4616ce2c4421045b
charles-mrt <charlesmartins.ads@gmail.com>
2023-09-04 14:10:12 -0300

    feat: Added code to extract data from the HTTP response

--- src/app/services/user/create-user.ts
+++ src/app/services/user/create-user.ts
@@ -7,13 +7,19 @@ interface UserProps {
 
 export const createUser = async (formData: UserProps) => {
   try {
-    const response = await api.post(`games/${formData.gameId}/players`, {      
+    const response = await api.post(`games/${formData.gameId}/players`, {
       name: formData.user_name
     })
-    if (response.status === 201) return true
-  
+    
+    if (response.status === 201) {
+      const userId = response.data.id
+      const userName = response.data.name
+      const userVote = response.data.vote
+      return { success: true, userId, userName, userVote }
+    }
+
   } catch (error) {
     console.error("error sending data to backend", error)
-    throw new Error("(create user) Erro interno, tente novamente")
+    throw new Error("Erro interno, tente novamente")
   }
 }
