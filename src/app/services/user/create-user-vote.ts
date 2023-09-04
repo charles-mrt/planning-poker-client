ef36a6aaad7920d4704ff2569e1f3f04640b6f5c
charles-mrt <charlesmartins.ads@gmail.com>
2023-09-04 14:11:08 -0300

    feat: implement player vote

--- src/app/services/user/create-user-vote.ts
+++ src/app/services/user/create-user-vote.ts
@@ -7,4 +7,19 @@ interface createUserVote {
 }
 
 export const createUserVote = async ({ gameId, playerId, vote }: createUserVote) => {
+
+  try {
+    const response = await api.patch(`games/${gameId}/players/${playerId}/vote/`, vote)
+
+    if (response.status === 200) {
+      const playerId = response.data.id
+      const playerName = response.data.name
+      const playerVote = response.data.vote
+      return { playerId, playerName, playerVote }
+    }
+    
+  } catch (error) {
+    console.error("(player vote) - error sending data to backend", error)
+    throw new Error("Erro interno, tente novamente")
+  }
 }
