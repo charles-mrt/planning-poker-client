c13f60f8a33e1f5d740ecbaed9ff0dce4930af31
charles-mrt <charlesmartins.ads@gmail.com>
2023-09-04 14:17:11 -0300

    feat: Implement component for displaying player votes

---
+++ src/app/components/Card/Players-voted-card.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image"
+import card from '/public/assets/card-cover.png'
+
+interface PlayersVotedCardProps {
+  cardRevealed: boolean
+  playerVote: string
+  playerName: string
+}
+
+export const PlayersVotedCard = ({ cardRevealed, playerVote, playerName }: PlayersVotedCardProps) => {
+  return (
+    <>
+      {!cardRevealed ? (
+
+        <div className="custom-transition w-10 h-16 rounded-md border-2 border-gray-300 bg-gradient-to-t from-gray-400 to-gray-200">
+          <Image
+            src={card}
+            alt="card"
+            className="object-center w-full h-full"
+          />
+        </div>
+
+      ) : (
+
+        <div
+          id="flip-card"
+          className=" w-10 h-16 rounded-[4px] flex items-center justify-center border-2 bg-gradient-to-b from-cyan-500 to-blue-500 -translate-y-2  border-blue-500 cursor-pointer">
+          <span id="flip-card" className="text-white font-bold text-xl uppercase">
+            {playerVote}
+          </span>
+        </div>
+      )}
+      
+      <span className="whitespace-pre-line w-10 text-xs text-center capitalize">
+        {playerName}
+      </span>
+
+    </>
+  )
+}
