fc4b7dc401bb0e03a734737530542bd2973bcb32
charles-mrt <charlesmartins.ads@gmail.com>
2023-09-04 14:18:57 -0300

    style: Update card component with new card color

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -7,8 +7,10 @@ interface CardProps {
 export const Card = ({ name, isSelected, onCardClick }: CardProps) => {
   return (
     <div
-      className={`${isSelected ? "bg-indigo-200 -translate-y-2 " : "bg-transparent"
-        } w-10 h-20 rounded-md border-2 border-indigo-600 text-indigo-600 ease-in duration-150 flex items-center justify-center text-xl uppercase cursor-pointer`}
+      className={`${isSelected 
+        ? "bg-gradient-to-b from-cyan-500 to-blue-500 -translate-y-2 font-bold text-white" 
+        : "bg-transparent text-blue-600"
+        } w-10 h-20 rounded-md border-2 border-blue-600  ease-in duration-150 flex items-center justify-center text-xl uppercase cursor-pointer hover:bg-blue-300 hover:font-bold`}
       onClick={onCardClick}
     >
       {name}
